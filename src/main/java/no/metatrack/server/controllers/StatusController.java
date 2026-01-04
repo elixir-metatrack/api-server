17adfd77eae279b3b90c9d5823736fdcc063e714
Joshua Baskaran <joshbaskaran@gmail.com>
2026-01-04 10:55:25 +0100

    chore: add logs to server health controller

--- src/main/java/no/metatrack/server/controllers/StatusController.java
+++ src/main/java/no/metatrack/server/controllers/StatusController.java
@@ -5,13 +5,17 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import no.metatrack.server.Dtos.HealthStatusResponse;
+import org.jboss.logging.Logger;
 
 @Path("/")
 public class StatusController {
+    private static final Logger log = Logger.getLogger(StatusController.class);
+
     @GET
     @Path("/status")
     @Produces(MediaType.APPLICATION_JSON)
     public HealthStatusResponse status() {
+        log.info("status:ok");
         return new HealthStatusResponse("OK");
     }
 }
