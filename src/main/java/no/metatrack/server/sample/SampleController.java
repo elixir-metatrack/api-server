0d06eab4058592568f4cd5fac8c06136fafc9313
Joshua Baskaran <joshbaskaran@gmail.com>
2026-01-23 15:46:13 +0100

    feat: perform a content type check and return early

--- src/main/java/no/metatrack/server/sample/SampleController.java
+++ src/main/java/no/metatrack/server/sample/SampleController.java
@@ -14,6 +14,7 @@ import no.metatrack.server.project.ProjectRoleCheck;
 import org.jboss.resteasy.reactive.RestForm;
 import org.jboss.resteasy.reactive.multipart.FileUpload;
 
+import java.nio.file.Files;
 import java.util.List;
 import java.util.UUID;
 
@@ -131,6 +132,15 @@ public class SampleController {
         if (!projectRoleCheck.isAtLeast(projectId, ProjectRole.EDITOR)) throw new ForbiddenException();
 
         if (file == null) throw new BadRequestException("No file uploaded");
+
+        try {
+            String contentType = Files.probeContentType(file.filePath());
+            if (contentType == null || (!contentType.equals("text/csv") && !contentType.equals("text/plain"))) {
+                throw new WebApplicationException("File content does not appear to be text/csv", 400);
+            }
+        } catch (Exception e) {
+            throw new BadRequestException("Could not determine content type of uploaded file");
+        }
 
         List<CSVUploadRowError> errors = csvSampleSheetImportService.importNewSamples(
                 projectId, file.filePath().toFile());
