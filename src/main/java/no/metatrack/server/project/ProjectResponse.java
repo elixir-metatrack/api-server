c9cf0c620901f25599905596a95ec2809457c564
Joshua Baskaran <joshbaskaran@gmail.com>
2026-03-06 09:38:01 +0100

    feat: return createdOn and modifiedOn timestamps in project response

--- src/main/java/no/metatrack/server/project/ProjectResponse.java
+++ src/main/java/no/metatrack/server/project/ProjectResponse.java
@@ -1,9 +1,12 @@
 package no.metatrack.server.project;
 
+import java.time.Instant;
 import java.util.UUID;
 
-public record ProjectResponse(Long id, String name, String description, UUID owner) {
+public record ProjectResponse(
+        Long id, String name, String description, UUID owner, Instant createdOn, Instant modifiedOn) {
     public static ProjectResponse fromEntity(Project project) {
-        return new ProjectResponse(project.id, project.name, project.description, project.owner);
+        return new ProjectResponse(
+                project.id, project.name, project.description, project.owner, project.createdOn, project.modifiedOn);
     }
 }
