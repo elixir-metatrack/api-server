03a7dbcc60131b01a87de7c18d78641e9ed002fd
Joshua Baskaran <joshbaskaran@gmail.com>
2026-04-14 13:30:47 +0200

    chore: enforce not null

--- src/main/java/no/metatrack/server/stats/MetatrackStatistics.java
+++ src/main/java/no/metatrack/server/stats/MetatrackStatistics.java
@@ -1,5 +1,8 @@
 package no.metatrack.server.stats;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.time.Instant;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record MetatrackStatistics(long projectCount, long sampleCount, long assayCount, Instant lastUpdated) {}
